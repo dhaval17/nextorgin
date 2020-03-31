ec77d7bc9ac11845362cb6c195d81d9ee5bd4b72
Dhaval Chauhan <dhaval@throughbit.com>
2020-03-31 21:44:23 +0530

    ínit

---
+++ js/init.js
@@ -0,0 +1,32 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function($) {
+
+
+/*----------------------------------------------------*/
+/*	Make sure that #header-background-image height is
+/* equal to the browser height.
+------------------------------------------------------ */
+
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function() {
+
+        $('header').css({ 'height': $(window).height() });
+        $('body').css({ 'width': $(window).width() })
+   });
+
+
+
+});
+
+
+
+
+
+
+
+
